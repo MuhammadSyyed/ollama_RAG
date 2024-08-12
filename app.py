5c212e37f621cba9e0fa690174338aeb92148461
Muhammad Imran <59409327+MuhammadSyyed@users.noreply.github.com>
2024-08-13 00:33:30 +0500

    Adds chat.html in templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import jsonify
 from flask import request
 from flask_cors import CORS
@@ -15,22 +15,23 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+@app.route("/", methods=['GET'])
+def chat_page():
+    return render_template('chat.html')
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     try:
         data = request.json
         chat_id = data['_id']
-        print(data)
 
         if data["messages"]:
             if data["messages"][0]["u"]["username"] == data["agent"]["username"]:
                 pass
             else:
                 if check_message_age(data["messages"][0]["ts"]):
-
-                    resp = chat(data["messages"][0]["msg"],
-                                data['visitor']['name'])
-
+                    resp = chat(data["messages"][0]["msg"], chat_id)
                     send_message_to_room(resp, data['_id'])
 
     except Exception as e:
@@ -46,10 +47,8 @@ def post_question():
     question = json['question']
     user_id = json['user_id']
     logging.info("post question `%s` for user `%s`", question, user_id)
-
     resp = chat(question, user_id)
     data = {'answer': resp}
-
     return jsonify(data), 200
 
 
@@ -57,5 +56,4 @@ if __name__ == '__main__':
     init_llm()
     index = init_index(Settings.embed_model)
     init_query_engine(index)
-
     app.run(host='0.0.0.0', port=HTTP_PORT, debug=True)
